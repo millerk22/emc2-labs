032b492f1df4c730e498e1f714904d84f5b9cff5
Shane <shanemcq@mathematics.byu.edu>
2025-08-25 15:52:00 -0600

    resolve README merge

--- EMC2-Labs-Winter/conf.py
+++ EMC2-Labs-Winter/conf.py
@@ -6,39 +6,43 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Winter EMC2 Labs'
-copyright = '2024, Nick Andersen'
-author = 'Nick Andersen'
-release = '2024'
+project = "Winter EMC2 Labs"
+copyright = "2024, Nick Andersen and EMC2 Developers"
+author = "Nick Andersen and EMC2 Developers"
+release = "2026"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", 'sphinx.ext.mathjax']
+extensions = ["myst_parser", "sphinx.ext.mathjax"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_material'
-html_static_path = ['_static']
+html_theme = "sphinx_material"
+html_static_path = ["_static"]
 
-html_title = 'Math 495R EMC2 Python Labs'
+html_title = "Math 495R EMC2 Python Labs"
 
 html_sidebars = {
-    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+    "**": [
+        "logo-text.html",
+        "globaltoc.html",
+        "localtoc.html",
+        "searchbox.html",
+    ]
 }
 
-html_theme_options = {
-	'nav_title': 'Winter EMC2 Labs'
-}	
+html_theme_options = {"nav_title": "Winter EMC2 Labs"}
 
 suppress_warnings = ["config.cache"]
 
-# allows for centering of figures, if things get messed up, this would be a good place to check
+
+# Allows for centering of figures.
+# If things get messed up, this would be a good place to check.
 def setup(app):
     app.add_css_file("custom.css")
